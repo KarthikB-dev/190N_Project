be0df6861b59ada537800df13845090b8e95ff7b
Tianle Yu <yulele28@126.com>
2024-12-13 14:29:28 -0800

    [feat] subset into sessions

--- data_util/src/subset.py
+++ data_util/src/subset.py
@@ -7,14 +7,29 @@ df = pd.read_csv(INPUT)
 
 # Get all the unique labels
 labels = df["label"].unique()
-print(labels)
 
+new_labels = []
+
+for label in labels:
+    if len(df[df["label"] == label]) > 500:
+        new_labels.append(label)
+
+labels = new_labels
+print("Selected labels", labels)
+dfs = []
+
+
+sessionId = 0
 
 def subset_labels(df, labels):
-    ndf = df[df["label"].isin(labels)]
-    OUTPUT = f"../data/output_subset_{len(labels)}_{",".join(list(labels))}.csv"
+    global sessionId
+    ndf = df[df["label"].isin(labels)].copy()
+    sessionId += 1
     if len(ndf) > 1000:
-        ndf.to_csv(OUTPUT, index=False)
+        print("Selected session", sessionId)
+        ndf.loc[:, 'label'] = len(labels)
+        ndf.loc[:, 'session'] = sessionId
+        dfs.append(ndf)
 
 
 # Generate all combinations of labels
@@ -22,3 +37,7 @@ for i in range(1, len(labels) + 1):
 
     for c in combinations(labels, i):
         subset_labels(df, c)
+
+df = pd.concat(dfs, ignore_index=True)
+OUTPUT = f"../data/output_subset.csv"
+df.to_csv(OUTPUT, index=False)
